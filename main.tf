b20ebb7efde194cd34b572db24790370599e3e6c
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-10-14 10:10:25 +0530

    Roboshop with terraform setup

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 resource "aws_subnet" "subnet" {
   vpc_id     = var.vpc_id
   cidr_block = var.cidr_block
-  for_each = var.subnets_name
 
   tags = {
-    Name = each.key-${var.env}
+    Name = "${var.subnets_name}-${var.env}-subnet"
   }
   }
